d3ec2b03e059759362a7191c6207f436f077f1f6
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-18 09:58:09 +0900

    Home Search　ビュー実装

---
+++ src/pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+	<div id="home">
+		<div>
+			<h2>Vue.jsで始めるSPA<h2>
+		</div>
+	</div>
+</template>
---
+++ src/pages/SerachIp.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>ここにIPが表示されます</h2>
+    <div>
+      <input type="button" value="IPを取得する" />
+    </div>
+  </div>
+</template>
